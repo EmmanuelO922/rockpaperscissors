1cff847144c95135d7228e9e3d7444b151cfdb98
Glitch (roc-papr-scisr) <none>
2021-12-01 23:06:36 +0000

    📯🌧 Updated thru Glitch by Emmanuel

--- js/script.js
+++ js/script.js
@@ -3,15 +3,21 @@ let randNum;
 $("button").click(function(){
   let userInput = $("input").val();
     $(".userChoice").text(userInput);
-  randNum = Math.floor(Math.random()*3);
+  randNum = Math.floor(Math.random()*(3));
     $(".computerChoice").text(randNum);
-//if(randNum = 0){
+if(randNum === 0){
   $(".computerChoice").text("rock");
-//}
-//else if(randNum = 1){
+}
+else if(randNum === 1){
   $(".computerChoice").text("paper");
-//}
-//else{
+}
+else if(randNum === 2){
   $(".computerChoice").text("scissors");
-//};
+}
+if(userInput !== "rock" && userInput !== "paper" && userInput !== "scissors"){
+  $(".computerChoice").text("invalid response")
+}
+if(".userChoice" === ".computerChoice"){
+  $(".result").text("TIE")
+}
 });
