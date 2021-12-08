6272407cd119820f0bcdea62b9ac9b0147a0941b
Glitch (roc-papr-scisr) <none>
2021-12-08 23:24:08 +0000

    🕷🚩 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,23 +1,78 @@
-let randNum;
+//let randNum;
+
+//$("button").click(function(){
+//  let userInput = $("input").val();
+//    $(".userChoice").text(userInput);
+// randNum = Math.floor(Math.random()*(3));
+//   $(".computerChoice").text(randNum);
+//  let computerPlay = $(".computerChoice").text;
+//  let userPlay = $(".userChoice").text;
+//  let winner = "";
+//if(randNum === 0){
+//  $(".computerChoice").text("rock");
+//}
+//else if(randNum === 1){
+//  $(".computerChoice").text("paper");
+//}
+//else if(randNum === 2){
+//  $(".computerChoice").text("scissors");
+//}
+//if(userInput !== "rock" && userInput !== "paper" && userInput !== "scissors"){
+//  $(".computerChoice").text("invalid response")
+//  $(".userChoice").text("invalid response")
+//}
+//Result
+
+//$(".result").text = $(".computerChoice")
+//});
+
+let choice = "";
+let randNumb;
+let compChoice = "";
 
 $("button").click(function(){
-  let userInput = $("input").val();
-    $(".userChoice").text(userInput);
-  randNum = Math.floor(Math.random()*(3));
-    $(".computerChoice").text(randNum);
-if(randNum === 0){
-  $(".computerChoice").text("rock");
-}
-else if(randNum === 1){
-  $(".computerChoice").text("paper");
-}
-else if(randNum === 2){
-  $(".computerChoice").text("scissors");
-}
-if(userInput !== "rock" && userInput !== "paper" && userInput !== "scissors"){
-  $(".computerChoice").text("invalid response")
-}
-if(".userChoice" === ".computerChoice"){
-  $(".result").text("TIE")
-}
+  let choice = $("input").val();
+  if(choice !== "rock" && choice !== "paper" && choice !== "scissors"){
+    $(".computerChoice").text("invalid response")
+    $(".userChoice").text("invalid response")
+  }
+  
+  $(".userPlay").text(choice);
+  randNumb = Math.floor(Math.random()*3);
+  $(".computerPlay").text(randNumb);
+  
+  if(randNumb == 0){
+    compChoice = "rock"
+  }
+  else if(randNumb == 1){
+    compChoice = "paper"
+  }
+  else if(randNumb == 2){
+    compChoice = "scissors"
+  }
+  
+  let winner = "";
+  
+  if (compChoice === "rock" && choice === "paper"){
+    
+  }
+  else if (compChoice === "scissors" && choice === "rock"){
+    
+  }
+  else if (compChoice === "paper" && choice === "scissors"){
+    
+  }
+  else if (compChoice === "rock" && choice === "scissors"){
+    
+  }
+  else if (compChoice === "paper" && choice === "rock"){
+    
+  }
+  else if (compChoice === "scissors" && choice === "paper"){
+    
+  }
+  
+  $(".result").text(winner);
+  $(".computerPlay").text(compChoice);
+  $(".userPlay").text();
 });
